3580bac475f40bd953e78889812864eabebe497b
gauchewy <90925919+gauchewy@users.noreply.github.com>
2023-11-02 17:48:32 -0400

    add in fran changes -binoculars and wave

--- Gestures3/ViewController.swift
+++ Gestures3/ViewController.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Qiwei on 11/2/23.
 //
-
 import UIKit
 import AVFoundation
 import Vision
@@ -31,6 +30,7 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     private lazy var previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
     
     private var previousIndexFingerCoordinates: [VNHumanHandPoseObservation.JointName: CGPoint] = [:]
+    private var previousWristLocations: [CGPoint] = []
     
     // MARK: - Lifecycle
     
@@ -59,21 +59,188 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
                 return print("Error: \(error?.localizedDescription ?? "unknown error")")
             }
             
+            // WAVE -- WORKS
             if self?.selectedOption == .wave {
                 if observations.count == 2 {
-                    DispatchQueue.main.async {
+                    var bothHandsExtended = true
+                    for observation in observations {
 
-                        self?.complete(true)
+                        guard let handExtended = self?.allFingersExtended(observation: observation) else {
+                            bothHandsExtended = false
+                            break
+                        }
+
+                        if !(handExtended) {
+                            bothHandsExtended = false
+                            break
+                        }
+
+                        guard let wrist = try? observation.recognizedPoints(.all)[.wrist] else {
+                            DispatchQueue.main.async {
+                                self?.complete(false)
+                            }
+                            return
+                        }
+                            
+                        let wristLocation = wrist.location
+                            
+                        if let previousWristLocation = self?.previousWristLocations.last {
+                            let threshold: CGFloat = 0.25
+                            let distance = sqrt(pow(wristLocation.x - previousWristLocation.x, 2) + pow(wristLocation.y - previousWristLocation.y, 2))
+                            if distance < threshold {
+                                print("UNDER THRESHOLD DISTANCE:+ \(distance)")
+                                DispatchQueue.main.async {
+                                    self?.complete(false)
+                                }
+                                return
+                            }
+                            print("OVER THRESHOLD DISTANCE:+ \(distance)")
+                        }
+                            
+                        self?.previousWristLocations.append(wristLocation)
+                        //print("wrist location:+ \(wristLocation)")
                     }
-                }
-                else {
+
                     DispatchQueue.main.async {
-                       
+                        // If all fingers are extended in both hands, then the user is performing the 'wave' gesture
+                        self?.complete(bothHandsExtended)
+                    }
+                } else {
+                    DispatchQueue.main.async {
                         self?.complete(false)
                     }
                 }
             }
-            // other conditions
+            
+            // BINOCULARS -- WORKS
+            if self?.selectedOption == .binoculars {
+                if observations.count == 2 {
+                    // thumbTip array so we can calculate the distance between the two hands
+                    var thumbTipLocations = [CGPoint]()
+                    for observation in observations {
+                        guard let thumbTip = try? observation.recognizedPoint(.thumbTip),
+                              let indexTip = try? observation.recognizedPoint(.indexTip),
+                              let middleTip = try? observation.recognizedPoint(.middleTip),
+                              let ringTip = try? observation.recognizedPoint(.ringTip),
+                              let littleTip = try? observation.recognizedPoint(.littleTip) else {
+                            continue
+                        }
+                        thumbTipLocations.append(thumbTip.location)
+                        
+                        // Calculate the distance between the thumb tip and index tip to check for binoculars pose
+                        let thumbIndexDistance = sqrt(pow(indexTip.location.x - thumbTip.location.x, 2) + pow(indexTip.location.y - thumbTip.location.y, 2))
+                        let thumbMiddleDist = sqrt(pow(middleTip.location.x - thumbTip.location.x, 2) + pow(middleTip.location.y - thumbTip.location.y, 2))
+                        let thumbRingDist = sqrt(pow(ringTip.location.x - thumbTip.location.x, 2) + pow(ringTip.location.y - thumbTip.location.y, 2))
+                        let thumbLittleDist = sqrt(pow(littleTip.location.x - thumbTip.location.x, 2) + pow(littleTip.location.y - thumbTip.location.y, 2))
+                        
+                        // If fingers are in binoculars pose, keep going
+                        if thumbIndexDistance < 0.05 && thumbMiddleDist < 0.1 && thumbRingDist < 0.1 && thumbLittleDist < 0.1 {
+                            continue
+                        } else {
+                            DispatchQueue.main.async {
+                                self?.complete(false)
+                            }
+                        }
+                    }
+                        
+                        // Calculate the distance between the thumbs
+                        let thumbTipDistance = sqrt(pow(thumbTipLocations[0].x - thumbTipLocations[1].x, 2) + pow(thumbTipLocations[0].y - thumbTipLocations[1].y, 2))
+                        
+                        
+                        // If two hands are close together, complete
+                        if thumbTipDistance < 0.05 {
+                            DispatchQueue.main.async {
+                                self?.complete(true)
+                            }
+                            
+                        } else {
+                            DispatchQueue.main.async {
+                                self?.complete(false)
+                            }
+                        }
+                }
+                // two hands not detected
+                else {
+                    DispatchQueue.main.async {
+                        self?.complete(false)
+                    }
+                }
+            }
+                        
+            
+            // HAND CLASP
+            if self?.selectedOption == .handClasp {
+                
+                for observation in observations {
+                    guard let thumbTip = try? observation.recognizedPoint(.thumbTip),
+                          let indexTip = try? observation.recognizedPoint(.indexTip),
+                          let middleTip = try? observation.recognizedPoint(.middleTip),
+                          let ringTip = try? observation.recognizedPoint(.ringTip),
+                          let littleTip = try? observation.recognizedPoint(.littleTip) else {
+                        continue
+                    }
+                    
+                    // Distances calculated directly
+                    let thumbIndexDist = sqrt(pow(indexTip.location.x - thumbTip.location.x, 2) + pow(indexTip.location.y - thumbTip.location.y, 2))
+                    let thumbMiddleDist = sqrt(pow(middleTip.location.x - thumbTip.location.x, 2) + pow(middleTip.location.y - thumbTip.location.y, 2))
+                    let thumbRingDist = sqrt(pow(ringTip.location.x - thumbTip.location.x, 2) + pow(ringTip.location.y - thumbTip.location.y, 2))
+                    let thumbLittleDist = sqrt(pow(littleTip.location.x - thumbTip.location.x, 2) + pow(littleTip.location.y - thumbTip.location.y, 2))
+                    
+                    // Check if all distances are under threshold
+                    let distances = [thumbIndexDist, thumbMiddleDist, thumbRingDist, thumbLittleDist]
+                    if distances.allSatisfy({ $0 < 0.2 }) {
+                        DispatchQueue.main.async {
+                            self?.complete(true)
+                        }
+                    } else {
+                        DispatchQueue.main.async {
+                            self?.complete(false)
+                        }
+                    }
+                }
+            }
+        
+   
+                
+
+            // INTERLACE
+            if self?.selectedOption == .interlace {
+                if observations.count == 2 {
+                    let knuckles: [VNHumanHandPoseObservation.JointName] = [
+                        .thumbIP, .indexPIP, .middlePIP, .ringPIP, .littlePIP
+                    ]
+                    
+                    // Check for valid knuckle points for each hand
+                    guard let points1 = try? observations[0].recognizedPoints(.all),
+                          let points2 = try? observations[1].recognizedPoints(.all) else {
+                        DispatchQueue.main.async {
+                            self?.complete(false)
+                        }
+                        return
+                    }
+
+                    // Check if most of the knuckles of two hands are close to each other
+                    var closeKnuckles = 0
+                    for knuckle in knuckles {
+                        guard let point1 = points1[knuckle], let point2 = points2[knuckle] else { continue }
+                            
+                        let distance = sqrt(pow(point1.location.x - point2.location.x, 2)
+                                          + pow(point1.location.y - point2.location.y, 2))
+                            
+                        if distance < 0.4 { // threshold is a predefined distance
+                            closeKnuckles += 1
+                        }
+                    }
+                    DispatchQueue.main.async{
+                        self?.complete(closeKnuckles >= 4) // expect at least 4 knuckles to be close
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        self?.complete(false)
+                    }
+                }
+            }
+          
         }
         self.requests.append(handPoseRequest)
     }
@@ -85,7 +252,7 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     // MARK: - Setup Functions
     private func addCameraInput() {
         guard let device = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInTrueDepthCamera, .builtInDualCamera, .builtInWideAngleCamera], mediaType: .video, position: .front).devices.first else {
-            fatalError("No camera detected. Please use a real camera, not a simulator.")
+            fatalError("No camera detected. Please use a real camera, not a siwewmulator.")
         }
 
         let cameraInput = try! AVCaptureDeviceInput(device: device)
@@ -115,6 +282,28 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         
     }
     
+    // repurposed method from gestures1
+    // returns true if all fingers are extended
+    private func allFingersExtended(observation: VNHumanHandPoseObservation) -> Bool? {
+        guard let thumbPoints = try? observation.recognizedPoints(.thumb),
+              let indexPoints = try? observation.recognizedPoints(.indexFinger),
+              let middlePoints = try? observation.recognizedPoints(.middleFinger),
+              let ringPoints = try? observation.recognizedPoints(.ringFinger),
+              let littlePoints = try? observation.recognizedPoints(.littleFinger),
+              let wristPoints = try? observation.recognizedPoints(.all),
+              let wrist = wristPoints[.wrist] else {
+            return nil
+        }
+
+        let thumbExtended = isFingerExtended(fingerTip: thumbPoints[.thumbTip], fingerPoints: thumbPoints, mcpJoint: .thumbMP, wristLocation: wrist.location)
+        let indexExtended = isFingerExtended(fingerTip: indexPoints[.indexTip], fingerPoints: indexPoints, mcpJoint: .indexMCP, wristLocation: wrist.location)
+        let middleExtended = isFingerExtended(fingerTip: middlePoints[.middleTip], fingerPoints: middlePoints, mcpJoint: .middleMCP, wristLocation: wrist.location)
+        let ringExtended = isFingerExtended(fingerTip: ringPoints[.ringTip], fingerPoints: ringPoints, mcpJoint: .ringMCP, wristLocation: wrist.location)
+        let littleExtended = isFingerExtended(fingerTip: littlePoints[.littleTip], fingerPoints: littlePoints, mcpJoint: .littleMCP, wristLocation: wrist.location)
+        
+        return thumbExtended && indexExtended && middleExtended && ringExtended && littleExtended
+    }
+    
     private func printExtendedFingers(observation: VNHumanHandPoseObservation, wristLocation: CGPoint) {
         // This dictionary stores the mapping from finger names to their corresponding points
         let fingerJoints: [String: (tip: VNHumanHandPoseObservation.JointName, mcp: VNHumanHandPoseObservation.JointName)] = [
@@ -138,7 +327,7 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
             )
             output += "\(fingerName): \(extended ? "Extended" : "Not Extended"), "
         }
-        print(output)
+        //print(output)
     }
     
     private func isFingerExtended(fingerTip: VNRecognizedPoint?,

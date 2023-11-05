e1adbcb91f9849039983df982579d68bbf8dc930
gauchewy <90925919+gauchewy@users.noreply.github.com>
2023-11-04 20:53:11 -0400

    q

--- Gestures3/HoldButtonsView.swift
+++ Gestures3/HoldButtonsView.swift
@@ -19,7 +19,7 @@ struct HoldButtonsView: View {
                         .aspectRatio(contentMode: .fit)
                         .padding()
                     
-                    if !viewCleared{
+                    if !viewCleared {
                         Rectangle()
                             .fill(Color.green)
                             .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -41,26 +41,47 @@ struct StackButtons: View {
     let width: CGFloat
     let height: CGFloat
     let buttonwidth = 50.0
-    
+    let buttonRadius = 50.0
+    let vShape = [0.05, 0.2, 0.4]
+    let growRatio = 1.05
+    @State private var highlightedIndex = 0
+
     var body: some View {
         VStack(spacing: 20) {
-            ForEach([0.3, 0.2, 0.1], id: \.self) { spacerRatio in
+            ForEach(vShape.indices, id: \.self) { index in
                 HStack {
-                    Spacer().frame(width: width * CGFloat(spacerRatio)-CGFloat(buttonwidth / 2))
+                    Spacer().frame(width: width * CGFloat(vShape[index])-CGFloat(buttonwidth / 2))
                     Button(action: {}){
-                        Circle().frame(width: buttonwidth, height: buttonwidth)
+                        Circle()
+                            .frame(width: highlightedIndex == index ? buttonwidth * growRatio : buttonwidth,
+                                   height: highlightedIndex == index ? buttonRadius * growRatio : buttonRadius)
+                            .foregroundColor(highlightedIndex == index ? .blue : .white)
                     }
-                    Spacer().frame(width: width * (0.5 - spacerRatio))
+                    Spacer().frame(width: width * (0.5 - vShape[index]))
                     Button(action: {}){
-                        Circle().frame(width: buttonwidth, height: buttonwidth)
+                        Circle()
+                            .frame(width: highlightedIndex == index ? buttonwidth * growRatio : buttonwidth,
+                                   height: highlightedIndex == index ? buttonRadius * growRatio : buttonRadius)
+                            .foregroundColor(highlightedIndex == index ? .blue : .white)
                     }
-                    Spacer().frame(width: width * spacerRatio)
+                    Spacer().frame(width: width * vShape[index])
+                }
+            }
+        }
+        .onAppear {
+            Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { _ in
+                // Animate the index increasing
+                withAnimation {
+                    if highlightedIndex < vShape.count - 1 {
+                        highlightedIndex += 1
+                    } else {
+                        highlightedIndex = 0
+                    }
                 }
             }
         }
     }
 }
-
 
 struct HoldButtonsView_Previews: PreviewProvider {
     static var previews: some View {

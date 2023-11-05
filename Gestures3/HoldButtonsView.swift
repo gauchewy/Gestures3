9887dac46e8ee13a8eaa1df033388850661f964f
gauchewy <90925919+gauchewy@users.noreply.github.com>
2023-11-04 20:09:47 -0400

    row buttons, misaligned

--- Gestures3/HoldButtonsView.swift
+++ Gestures3/HoldButtonsView.swift
@@ -6,86 +6,64 @@
 //
 
 import SwiftUI
-import UIKit
 
-struct HoldButtonViewControllerRepresentable: UIViewControllerRepresentable {
-    typealias UIViewControllerType = HoldButtonsViewController
+struct HoldButtonsView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Spacer()
+            StackButtons()
 
-    func makeUIViewController(context: Context) -> HoldButtonsViewController {
-        return HoldButtonsViewController()
-    }
-
-    func updateUIViewController(_ uiViewController: HoldButtonsViewController, context: Context) {
+        }
     }
 }
 
-class HoldButtonsView: UIView {
-    
-    var path: UIBezierPath!
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        self.backgroundColor = UIColor.white
-        path = UIBezierPath()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let touchPoint = touches.first! as UITouch
-        let location = touchPoint.location(in: self)
-        
-        path.move(to: location)
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let touchPoint = touches.first! as UITouch
-        let location = touchPoint.location(in: self)
-        
-        path.addLine(to: location)
-        self.setNeedsDisplay()
-    }
-    
-    override func draw(_ rect: CGRect) {
-        path.lineWidth = 2.0
-        UIColor.red.setStroke()
-        path.stroke()
+struct StackButtons: View {
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer().frame(width: geometry.size.width * 0.15)
+                    Button(action: {}){
+                        Circle().frame(width: 50, height: 50)
+                    }
+                    Spacer().frame(width: geometry.size.width * 0.70)
+                    Button(action: {}){
+                        Circle().frame(width: 50, height: 50)
+                    }
+                    Spacer().frame(width: geometry.size.width * 0.15)
+                }
+                HStack {
+                    Spacer().frame(width: geometry.size.width * 0.25)
+                    Button(action: {}){
+                        Circle().frame(width: 50, height: 50)
+                    }
+                    Spacer().frame(width: geometry.size.width * 0.50)
+                    Button(action: {}){
+                        Circle().frame(width: 50, height: 50)
+                    }
+                    Spacer().frame(width: geometry.size.width * 0.25)
+                }
+                HStack {
+                    Spacer().frame(width: geometry.size.width * 0.35)
+                    Button(action: {}){
+                        Circle().frame(width: 50, height: 50)
+                    }
+                    Spacer().frame(width: geometry.size.width * 0.30)
+                    Button(action: {}){
+                        Circle().frame(width: 50, height: 50)
+                    }
+                    Spacer().frame(width: geometry.size.width * 0.35)
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+        }
     }
 }
 
-class HoldButtonsViewController: UIViewController {
-    
-    var drawView: HoldButtonsView!
-    var animateButton: UIButton!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // DrawView
-        drawView = HoldButtonsView(frame: self.view.frame)
-        self.view.addSubview(drawView)
-        
-        // Animate Button
-        let buttonSize: CGFloat = 50
-        animateButton = UIButton(frame: CGRect(x: buttonSize/2, y: buttonSize/2, width: buttonSize, height: buttonSize))
-        animateButton.backgroundColor = .blue
-        animateButton.layer.cornerRadius = buttonSize/2
-
-        animateButton.addTarget(self, action: #selector(animateButtonAction), for: .touchUpInside)
-        
-        self.view.addSubview(animateButton)
-    }
-    
-    @objc func animateButtonAction() {
-        let animation = CAKeyframeAnimation(keyPath: #keyPath(CALayer.position))
-        animation.path = drawView.path.cgPath
-        animation.speed = 0.5
-        animation.repeatCount = .infinity
-        animation.autoreverses = true
-        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
-        
-        animateButton.layer.add(animation, forKey: "animation")
+struct HoldButtonsView_Previews: PreviewProvider {
+    static var previews: some View {
+        HoldButtonsView()
     }
 }

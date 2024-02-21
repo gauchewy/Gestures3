e6317f5389d3514fcd2fb07341cad5fdaae11224
gauchewy <90925919+gauchewy@users.noreply.github.com>
2024-02-21 15:05:04 -0500

    wip detectr and move on func

--- Gestures3/TaskMainView.swift
+++ Gestures3/TaskMainView.swift
@@ -21,7 +21,10 @@ struct TaskMainView: View {
     @State private var isParticipantNumberEntered: Bool = false
     
     @State private var resetKey = UUID()
-
+    @State private var progressMethod: ProgressOption = .screenshot
+    
+    @State private var isPoseDetected: Bool = false
+    
     
     init() {
         _type = State(initialValue: type.shuffled())
@@ -29,42 +32,57 @@ struct TaskMainView: View {
         _offGestures = State(initialValue: offGestures.shuffled())
     }
     
+    enum ProgressOption: String, CaseIterable, Identifiable {
+        case screenshot = "Screenshot"
+        case nextButton = "Next Button"
+        case detectPose = "Detect Pose"
+        
+        var id: String { self.rawValue }
+    }
+    
     
     var body: some View {
         
         NavigationView {
-                   VStack {
-                       if !isParticipantNumberEntered {
-                           participantNumberInputView()
-                       } else {
-                           gestureTaskView()
-                       }
-                   }
-               }
+            VStack {
+                if !isParticipantNumberEntered {
+                    participantNumberInputView()
+                } else {
+                    gestureTaskView()
+                }
+            }
+        }
         
     }
     
     private func participantNumberInputView() -> some View {
-            VStack {
-                Text("Enter Participant ID")
-                    .font(.title)
-                TextField("Enter Participant Number", value: $currentParticipantNumber, formatter: NumberFormatter())
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .keyboardType(.numberPad)
-                    .padding()
-
-                Button("Submit") {
-                    isParticipantNumberEntered = true
-                }
+        VStack {
+            Text("Enter Participant ID")
+                .font(.title)
+            TextField("Enter Participant Number", value: $currentParticipantNumber, formatter: NumberFormatter())
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .keyboardType(.numberPad)
                 .padding()
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .cornerRadius(8)
+            
+            Picker("Progress Method", selection: $progressMethod) {
+                ForEach(ProgressOption.allCases) { option in
+                    Text(option.rawValue).tag(option)                }
             }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding()
+            
+            Button("Submit") {
+                print(progressMethod)
+                isParticipantNumberEntered = true
+            }
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(8)
         }
+    }
     
     private func gestureTaskView() -> some View {
-        
         NavigationView {
             VStack {
                 if type.first == "onGestures" {
@@ -84,87 +102,97 @@ struct TaskMainView: View {
                 if currentIndex >= onGestures.count + offGestures.count {
                     Text("All tasks completed")
                 }
+
+                Button("Next Gesture") {
+                    moveToNextGesture()
+                    
+                }
             }
             .onReceive(NotificationCenter.default.publisher(for: UIApplication.userDidTakeScreenshotNotification)) { _ in
                 isScreenshotTaken = true
             }
             .onChange(of: isScreenshotTaken) { taken in
-                if taken {
-                    // currently this is the only way to move on
+                if taken && progressMethod == .screenshot {
                     moveToNextGesture()
                 }
             }
+            .onChange(of: isPoseDetected) { detected in
+                if detected && progressMethod == .detectPose {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        moveToNextGesture()
+                    }
+                }
+            }
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
-
 
         func gestureView(for gesture: String) -> some View {
             switch gesture {
                 // on gestures
-               case "tap 1":
-                   return AnyView(TapButtonsViewV1(onComplete: moveToNextGesture))
-               case "tap 2":
-                   return AnyView(TapButtonsViewV2(onComplete: moveToNextGesture))
-               case "tap 3":
-                    return AnyView(TapButtonsViewV3(onComplete: moveToNextGesture))
-               case "slide":
-                    return AnyView(SlideButtonsView(onComplete: moveToNextGesture))
-               
+            case "tap 1":
+                return AnyView(TapButtonsViewV1(onComplete: moveToNextGesture))
+            case "tap 2":
+                return AnyView(TapButtonsViewV2(onComplete: moveToNextGesture))
+            case "tap 3":
+                return AnyView(TapButtonsViewV3(onComplete: moveToNextGesture))
+            case "slide":
+                return AnyView(SlideButtonsView(onComplete: moveToNextGesture))
+                
                 // off gestures
-               case "interlace":
+            case "interlace":
                 return AnyView(OtherView(selection: .interlace, onComplete: { data in
-                                    saveParticipantData(data)
-                                }).id(resetKey))
+                    saveParticipantData(data)
+                }).id(resetKey))
             case "binoculars":
                 return AnyView(OtherView(selection: .binoculars, onComplete: { data in
-                                    saveParticipantData(data)
-                                }).id(resetKey))
+                    saveParticipantData(data)
+                }).id(resetKey))
             case "wave":
                 return AnyView(OtherView(selection: .wave, onComplete: { data in
-                                    saveParticipantData(data)
-                                }).id(resetKey))
+                    saveParticipantData(data)
+                }).id(resetKey))
             case "frame":
                 return AnyView(OtherView(selection: .frame, onComplete: { data in
-                                    saveParticipantData(data)
-                                }).id(resetKey))
+                    saveParticipantData(data)
+                }).id(resetKey))
                 
                 
-               default:
-                   return AnyView(Text("Unknown Gesture"))
-               }
+            default:
+                return AnyView(Text("Unknown Gesture"))
+            }
         }
-    
-    func saveParticipantData(_ gestureData: [String: Any]) {
-        let participantData = ParticipantData(participantNumber: currentParticipantNumber, gestureData: gestureData)
-        participantDataList.append(participantData)
-        moveToNextParticipant()
-    }
-    
-    func moveToNextParticipant() {
-        if currentIndex >= onGestures.count + offGestures.count - 1 {
-            currentParticipantNumber += 1
-            currentIndex = 0 // Reset the index for the next participant
-            // Reset other states as needed for the next participant
-        } else {
-            moveToNextGesture()
+        
+        func saveParticipantData(_ gestureData: [String: Any]) {
+            let participantData = ParticipantData(participantNumber: currentParticipantNumber, gestureData: gestureData)
+            participantDataList.append(participantData)
+            moveToNextParticipant()
+        }
+        
+        func moveToNextParticipant() {
+            if currentIndex >= onGestures.count + offGestures.count - 1 {
+                currentParticipantNumber += 1
+                currentIndex = 0 // Reset the index for the next participant
+                // Reset other states as needed for the next participant
+            } else {
+                moveToNextGesture()
+            }
+        }
+        
+        func moveToNextGesture() {
+            if currentIndex < onGestures.count + offGestures.count - 1 {
+                currentIndex += 1
+                isScreenshotTaken = false
+                resetKey = UUID()
+            }
         }
     }
-
-    func moveToNextGesture() {
-           if currentIndex < onGestures.count + offGestures.count - 1 {
-               currentIndex += 1
-               isScreenshotTaken = false
-               resetKey = UUID()
-           }
-       }
+    
+    
+    
+    struct ParticipantData {
+        var participantNumber: Int
+        var gestureData: [String: Any]
     }
-#Preview {
-    TaskMainView()
-}
-
-
-struct ParticipantData {
-    var participantNumber: Int
-    var gestureData: [String: Any]
-}
+    
 

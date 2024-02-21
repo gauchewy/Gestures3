b9877a9b38819983ade9d5e772de60d397ef6dc4
gauchewy <90925919+gauchewy@users.noreply.github.com>
2024-02-20 20:53:54 -0500

    less buggy

--- Gestures3/ViewController.swift
+++ Gestures3/ViewController.swift
@@ -20,10 +20,12 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     var lastSquareDetectionTime: Date? = nil
     var lastWaveDetectionTime: Date? = nil
     var delayTime: Double = 2.0
+    var confLevel = 0.5 // vary this??? 
+    private var falseDetectionTimer: Timer?
     private var noObservationsTriggered = false
-    let gestureTimeoutInterval: TimeInterval = 0.0
+    //let gestureTimeoutInterval: TimeInterval = 1.0
     let complete: (Bool) -> Void
-
+    
     init(selectedOption: SelectedOption, completion: @escaping (Bool) -> Void) {
         self.selectedOption = selectedOption
         self.complete = completion
@@ -31,8 +33,8 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     }
     
     required init?(coder: NSCoder) {
-          fatalError("init(coder:) has not been implemented")
-      }
+        fatalError("init(coder:) has not been implemented")
+    }
     // MARK: - Variables
     private var requests = [VNRequest]()
     private let videoDataOutput = AVCaptureVideoDataOutput()
@@ -52,7 +54,7 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         config.computeUnits = .all
         
         gestureModel = loadModel()
-       
+        
         setupVision()
         addCameraInput()
         showCameraFeed()
@@ -68,79 +70,59 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         previewLayer.frame = view.frame
     }
     
-
+    
     
     private func setupVision() {
-
-        // Completion handler
+        
         let handler: ([VNHumanHandPoseObservation]) -> Void = { observations in
-            
-            // Reset the no observations trigger
-            self.noObservationsTriggered = false
-
-            if !observations.isEmpty {
-                guard let keypointsMultiArray = try? observations[0].keypointsMultiArray() else {
-                    print("Failed to create keypointsMultiArray")
-                    self.complete(false)
-                    return
-                }
-            } else {
-                print("No observations available")
-                if (!self.noObservationsTriggered){
-                    self.noObservationsTriggered = true
-                    DispatchQueue.main.asyncAfter(deadline: .now() + self.delayTime) { [weak self] in  // Change delay time as needed
-                        if self?.noObservationsTriggered ?? false {
-                            self?.complete(false)
-                        }
-                    }
-                }
+            if observations.isEmpty {
+                print("No hand poses detected")
+                self.complete(false)
                 return
             }
-
+            
             guard let keypointsMultiArray = try? observations[0].keypointsMultiArray() else {
                 print("Failed to create keypointsMultiArray")
                 self.complete(false)
                 return
             }
-
+            
             guard let predictionOutput = try? self.gestureModel.prediction(poses: keypointsMultiArray) else {
                 print("Failed to make prediction")
+                self.complete(false)
                 return
             }
-
-            let predictedLabel = predictionOutput.label // No need for optional binding
+            
+            let predictedLabel = predictionOutput.label
             guard let confidence = predictionOutput.labelProbabilities[predictedLabel] else {
                 print("Unable to retrieve confidence for \(predictedLabel)")
+                self.complete(false)
                 return
             }
-
+            
             print("PREDICTION: \(predictedLabel) \(confidence)")
-
+            
             DispatchQueue.main.async {
-                if self.selectedOption == .binoculars && predictedLabel == "Binoculars" && confidence >= 0.9 {
-                    self.lastBinocularsDetectionTime = Date()
+                // Check if the predicted gesture matches the selected gesture
+                switch self.selectedOption {
+                case .binoculars where predictedLabel == "Binoculars" && confidence >= self.confLevel,
+                        .binoculars where predictedLabel == "InterlaceFingers" && confidence >= self.confLevel,
+                        .interlace where predictedLabel == "InterlaceFingers" && confidence >= self.confLevel,
+                        .square where predictedLabel == "Square" && confidence >= self.confLevel,
+                        .wave where predictedLabel == "Wave" && confidence >= self.confLevel:
+                    self.falseDetectionTimer?.invalidate()
+                    self.falseDetectionTimer = nil
                     self.complete(true)
-                }
-                if self.selectedOption == .interlace && predictedLabel == "InterlaceFingers" && confidence >= 0.9 {
-                    self.lastInterlaceDetectionTime = Date()
-                    self.complete(true)
-                }
-                
-                if self.selectedOption == .square && predictedLabel == "Square" && confidence >= 0.9 {
-                    self.lastSquareDetectionTime = Date()
-                    self.complete(true)
-                }
-                
-                if self.selectedOption == .wave && predictedLabel == "Wave" && confidence >= 0.9 {
-                    self.lastWaveDetectionTime = Date()
-                    self.complete(true)
-                }
-                if predictedLabel == "Background" && confidence >= 0.9 {
+                    
+                default:
+                    // Complete with false if no gesture is detected or if another gesture is detected
                     self.complete(false)
+                    self.startOrResetFalseDetectionTimer()
                 }
             }
         }
-
+        
+        
         // Define hand pose request
         let handPoseRequest = VNDetectHumanHandPoseRequest(completionHandler: {(request, error) in
             guard let observations = request.results as? [VNHumanHandPoseObservation] else {
@@ -148,67 +130,78 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
             }
             handler(observations)
         })
-
+        
         self.requests.append(handPoseRequest)
     }
     
-  
-
-    // MARK: - Setup Functions
-    private func addCameraInput() {
-        guard let device = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInTrueDepthCamera, .builtInDualCamera, .builtInWideAngleCamera], mediaType: .video, position: .front).devices.first else {
-            fatalError("No camera detected. Please use a real camera, not a simulator.")
-        }
-
-        let cameraInput = try! AVCaptureDeviceInput(device: device)
-        captureSession.addInput(cameraInput)
-    }
     
-    private func showCameraFeed() {
-        previewLayer.videoGravity = .resizeAspectFill
-        view.layer.addSublayer(previewLayer)
-        previewLayer.frame = view.frame
-    }
-    
-    private func getCameraFrames() {
-        videoDataOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as NSString): NSNumber(value: kCVPixelFormatType_32BGRA)] as [String: Any]
+    private func startOrResetFalseDetectionTimer() {
+        // Invalidate and nullify any existing timer
+        self.falseDetectionTimer?.invalidate()
+        self.falseDetectionTimer = nil
         
-        videoDataOutput.alwaysDiscardsLateVideoFrames = true
-        // You do not want to process the frames on the Main Thread so we offload to another thread
-        videoDataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "camera_frame_processing_queue"))
-        
-        captureSession.addOutput(videoDataOutput)
-        
-        guard let connection = videoDataOutput.connection(with: .video), connection.isVideoOrientationSupported else {
-            return
-        }
-        
-        connection.videoOrientation = .portrait
-        
-    }
-    
-    func loadModel() -> GESTURES31 {
-        let config = MLModelConfiguration()
-        guard let model = try? GESTURES31(configuration: config) else {
-            fatalError("Failed to load the GESTURES31 model")
-        }
-        return model
-    }
-    
-    
-    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
-            return
-        }
-
-        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:])
-        do {
-            try imageRequestHandler.perform(self.requests)
-        } catch {
-            print("Failed to perform image request handler: \(error)")
-            return
+        // Start a new timer
+        self.falseDetectionTimer = Timer.scheduledTimer(withTimeInterval: self.delayTime, repeats: false) { _ in
+            self.complete(false)
         }
     }
-    
+        
+        
+        // MARK: - Setup Functions
+        private func addCameraInput() {
+            guard let device = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInTrueDepthCamera, .builtInDualCamera, .builtInWideAngleCamera], mediaType: .video, position: .front).devices.first else {
+                fatalError("No camera detected. Please use a real camera, not a simulator.")
+            }
+            
+            let cameraInput = try! AVCaptureDeviceInput(device: device)
+            captureSession.addInput(cameraInput)
+        }
+        
+        private func showCameraFeed() {
+            previewLayer.videoGravity = .resizeAspectFill
+            view.layer.addSublayer(previewLayer)
+            previewLayer.frame = view.frame
+        }
+        
+        private func getCameraFrames() {
+            videoDataOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as NSString): NSNumber(value: kCVPixelFormatType_32BGRA)] as [String: Any]
+            
+            videoDataOutput.alwaysDiscardsLateVideoFrames = true
+            // You do not want to process the frames on the Main Thread so we offload to another thread
+            videoDataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "camera_frame_processing_queue"))
+            
+            captureSession.addOutput(videoDataOutput)
+            
+            guard let connection = videoDataOutput.connection(with: .video), connection.isVideoOrientationSupported else {
+                return
+            }
+            
+            connection.videoOrientation = .portrait
+            
+        }
+        
+        func loadModel() -> GESTURES31 {
+            let config = MLModelConfiguration()
+            guard let model = try? GESTURES31(configuration: config) else {
+                fatalError("Failed to load the GESTURES31 model")
+            }
+            return model
+        }
+        
+        
+        func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+            guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+                return
+            }
+            
+            let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:])
+            do {
+                try imageRequestHandler.perform(self.requests)
+            } catch {
+                print("Failed to perform image request handler: \(error)")
+                return
+            }
+        }
+        
     }
 
